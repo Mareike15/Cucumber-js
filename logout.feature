aba9e5b3efea39f4be7e8bc3e64a2fde3b33e88a
Mareike15 <mareikemuus@gmail.com>
2017-01-31 17:37:59 +0100

    Add files via upload

--- logout.feature
+++ logout.feature
@@ -1,13 +1,9 @@
 Feature: Log Out
-  As a IBM Employee
+  As an IBM Employee
   I want to log out
   So that I can leave the Website of “DevOps Backbone Initiative”
 
   Scenario: Log Out
-    Given I am as a IBM Employee login on “DevOps Backbone Initiative”-Website
-    Given I insert my UserID
-    Given I insert my password
-    Given I click on "Sign In"-Button
-    Given I wait for "Your DevOps Experience"
+    Given I am logged into the DevOps Backbone Initiative-Website
     When I click on “Logout”-Button
-    Then I should see the Confirmation "You are now logged out"
+    Then I should see the Confirmation
--- logout_steps.js
+++ logout_steps.js
@@ -2,37 +2,23 @@ var seleniumWebdriver = require('selenium-Webdriver');
 var {defineSupportCode} = require('cucumber');
 
 defineSupportCode(function({Given, When, Then}) {
-Given('I am as a IBM Employee login on “DevOps Backbone Initiative”-Website', function (){
-  return this.driver.get("https://devopscoc.w3ibm.mybluemix.net/")
+
+Given('I am logged into the DevOps Backbone Initiative-Website', function (){
+  this.driver.get("https://devopscoc.w3ibm.mybluemix.net/");
+  this.driver.findElement({name: "username"}).sendKeys("mmuus@de.ibm.com");
+  this.driver.findElement({name: "password"}).sendKeys("Winter-02-17");
+  this.driver.findElement({id: "btn_signin"}).click();
+  var condition = seleniumWebdriver.until.titleContains("DevOps Backbone Initiative");
+   return this.driver.wait(condition.fn, 10000);
   });
-Given('I insert my UserID', function(){
-  return this.driver.findElement({name:"username"}).then(function(element){
-    return element.sendKeys("mmuus@de.ibm.com");
-    });
-  });
-Given('I insert my password', function(){
-  return this.driver.findElement({name:"password"}).then(function(element){
-    return element.sendKeys("password");
-    });
-  });
-Given('I click on "Sign In"-Button', function(){
-  return this.driver.findElement({id: "btn_signin"}).then(function(element) {
-    return element.click();
-    });
-  });
-Given('I wait for {text:stringInDoubleQuotes}', function(text){ //Text: Your DevOps Experience
-    var xpath = "//*[contains(text(),'" + text + "')]";
-    var condition = seleniumWebdriver.until.elementLocated({xpath: xpath});
-    return this.driver.wait(condition.fn, 10000); //Error --> Invalid Locator
-    });
+
   When('I click on “Logout”-Button', function(){
-    return this.driver.findElement({id:"logout"}).then(function(element){
-      return element.click();
-      });
-    });
-  Then('I should see the Confirmation {arg1:stringInDoubleQuotes}', function(arg1){
+    return this.driver.findElement({id:"logout"}).then(function(element) {
+      return element.click();});
+  });
+
+  Then('I should see the Confirmation', function(){
     var condition = seleniumWebdriver.until.titleContains("IBM W3 ID");
-    return this.driver.wait(condition.fn, 100000);
-    return this.driver.findElement({arg1});
+    return this.driver.wait(condition.fn, 10000);
     });
   });
